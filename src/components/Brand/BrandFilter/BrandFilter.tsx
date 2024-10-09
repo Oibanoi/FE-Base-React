8217a47dd27b08ed6b3f89ce87cf0451346c5af0
Phạm Bằng Hữu <huu.pb@teko.vn>
2024-10-09 14:12:07 +0700

    add unit test for component

--- src/components/Brand/BrandFilter/BrandFilter.tsx
+++ src/components/Brand/BrandFilter/BrandFilter.tsx
@@ -31,19 +31,33 @@ const BrandFilter: React.FC<BrandFilterProps> = (props) => {
       <Row gutter={16}>
         <Col span={8}>
           <Form.Item name="query" className="mb-0">
-            <Input autoFocus allowClear placeholder={t('EnterName')} />
+            <Input
+              autoFocus
+              allowClear
+              placeholder={t('EnterName')}
+              data-testid="EnterName"
+            />
           </Form.Item>
         </Col>
         <Col span={8}>
           <Form.Item name="isActive" className="mb-0">
-            <Select placeholder={t('SelectStatus')} allowClear>
+            <Select
+              placeholder={t('SelectStatus')}
+              allowClear
+              data-testid="SelectStatus"
+            >
               <Option value={1}>{t('Active')}</Option>
               <Option value={0}>{t('Inactive')}</Option>
             </Select>
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Button type="primary" htmlType="submit" icon={<SearchOutlined />}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            icon={<SearchOutlined />}
+            data-testid="Search"
+          >
             {t('Search')}
           </Button>
         </Col>
